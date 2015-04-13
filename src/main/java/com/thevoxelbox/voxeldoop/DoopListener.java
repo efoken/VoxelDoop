ce8c59e0904272a189bc7899a4b47d4f7a34b702
Eike Foken <e.foken@abian.de>
2015-04-13 16:23:06 +0200

    Remove item duplication on right click

--- src/main/java/com/thevoxelbox/voxeldoop/DoopListener.java
+++ src/main/java/com/thevoxelbox/voxeldoop/DoopListener.java
@@ -2,9 +2,7 @@ package com.thevoxelbox.voxeldoop;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.ItemStack;
 
 public class DoopListener implements Listener {
     private final VoxelDoop plugin;
@@ -13,27 +11,9 @@ public class DoopListener implements Listener {
         this.plugin = plugin;
     }
 
-    /* Feature incorporated into onPlayerInteract() to allow for use with placing items such as redstone.
-    @EventHandler
-    public void onBlockPlace(BlockPlaceEvent event)
-    {
-        ItemStack placedItem = event.getItemInHand();
-        if (placedItem != null)
-        {
-            if (placedItem.getAmount() == 32 && placedItem.getType().isBlock())
-            {
-                placedItem.setAmount(64);
-            }
-        }
-    }*/
-
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
         if (event.hasItem()) {
-            if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-                ItemStack hand = event.getPlayer().getItemInHand();
-                if (hand.getAmount() <= 32 && hand.getAmount() >= 2) hand.setAmount(64);
-            }
             if (event.getClickedBlock() == null) {
                 this.plugin.getToolManager().onRangedUse(event.getPlayer(), event.getItem(), event.getAction());
             } else {
